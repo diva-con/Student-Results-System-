5bc2630a2b687fafefa80399523d9cafe9217bcc
diva-con <preshiyemifokhae@gmail.com>
2026-01-21 19:46:49 +0100

    Add student result management system code

---
+++ student_results.py
@@ -0,0 +1,8 @@
+# Student Result Management System
+
+student_name = input("Enter student name: ")
+score = int(input("Enter student score: "))
+
+print("\n--- Student Result ---")
+print("Name:", student_name)
+print("Score:", score)
